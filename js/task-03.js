869f261f76aaa41c601fe2c4792f28ea8a427341
YevheniiDovhosheia <124459277+YevheniiDovhosheia@users.noreply.github.com>
2023-07-03 22:54:21 +0200

    chenges

--- js/task-03.js
+++ js/task-03.js
@@ -1,23 +1,24 @@
 const images = [
   {
-    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
     alt: "White and Black Long Fur Cat",
   },
   {
-    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
     alt: "Group of Horses Running",
   },
 ];
-const galleryList = document.querySelector(".gallery");
-const imageList = images
+const list = document.querySelector(".gallery");
+const items = images
   .map(
     ({ url, alt }) =>
-      `<li class="item"><img scr="${url}"alt="${alt}"widht="260"height="200"</li>`
+      `<li class="item"><img src='${url}'alt='${alt}'widht='260px';height='200px';></li>`
   )
   .join("");
-console.log(imageList);
-galleryList.insertAdjacentHTML("afterbegin", imageList);
+console.log(items);
+
+list.insertAdjacentHTML("afterbegin", items);
